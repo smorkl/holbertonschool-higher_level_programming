de9983bae711b003fe6ea84dc348bc7e659ade6c
smorkl <smsalazar0319@gmail.com>
2024-05-23 13:50:35 -0500

    create

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    if a > 0 and b > 0:
+    if add.a > 0 and add.b > 0:
         print("{} + {} = {}".format(a, b, add(a, b)))
