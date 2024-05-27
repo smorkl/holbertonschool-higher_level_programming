c9248ca61d85b9931d711aa1390dfc6c416b5c96
smorkl <smsalazar0319@gmail.com>
2024-05-27 13:13:56 -0500

    create

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
+    """Prints the integers in a list, one per line.
+
+    Args:
+        my_list (list, optional): The list of integers to print. Defaults to [].
+    """
     while i in my_list:
-        print("{}".format(i))
+        print(f"{i}")
