70a3a2b23166ff0ca89037047cfa5dd4d355f62a
smorkl <smsalazar0319@gmail.com>
2024-05-26 22:32:16 -0500

    create

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,22 +2,23 @@
 
 import sys
 
+
 def print_args(args):
-  """Prints information about the arguments passed to the script.
 
-  Args:
+    """Prints information about the arguments passed to the script.
+    Args:
     args: A list of strings representing the command-line arguments.
-  """
-
-  num_args = len(args) - 1
-  if num_args == 0:
-    print("0 arguments.")
-  elif num_args == 1:
-    print(f"1 argument:\n1: {args[1]}")
-  else:
-    print(f"{num_args} arguments:")
+    """
+    cant_args = len(args) - 1
+    if cant_args == 0:
+        print("0 arguments.")
+    elif cant_args == 1:
+        print(f"1 argument:\n1: {args[1]}")
+    else:
+        print(f"{cant_args} arguments:")
     for i in range(1, len(args)):
-      print(f"{i}: {args[i]}")
+        print(f"{i}: {args[i]}")
+
 
 if __name__ == "__main__":
-  print_args(sys.argv)
+    print_args(sys.argv)
