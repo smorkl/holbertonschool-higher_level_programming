9b485ab69cecc8f422d8dcbd6f1541ec33c574a6
smorkl <smsalazar0319@gmail.com>
2024-05-26 23:01:29 -0500

    create

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -8,8 +8,8 @@ def add_num(args):
     list_int = [int(x) for x in args[1:]]
     add_total = 0
     if len(list_int) > 1:
-        for add in list_int:
-            add_total += add
+        for numero in list_int:
+            add_total += numero
         print(f"{add_total}")
     else:
         print(len(args))
