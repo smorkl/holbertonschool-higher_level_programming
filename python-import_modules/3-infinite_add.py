e211098998a9ea5733abf5b55067c4f596a1c478
smorkl <smsalazar0319@gmail.com>
2024-05-26 23:00:18 -0500

    create

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -7,11 +7,12 @@ def add_num(args):
 
     list_int = [int(x) for x in args[1:]]
     add_total = 0
-    
     if len(list_int) > 1:
         for add in list_int:
             add_total += add
         print(f"{add_total}")
+    else:
+        print(len(args))
 
 
 if __name__ == "__main__":
