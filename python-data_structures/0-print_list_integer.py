a2604de6cc7d1ff366bc625d682a4fb8367d5ab6
smorkl <smsalazar0319@gmail.com>
2024-05-27 13:10:47 -0500

    create

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    while i in len(my_list):
-        print("{}".format(my_list[i]))
+    while i in my_list:
+        print("{}".format(i))
