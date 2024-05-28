205d956d0178b0ff83a2be3ff5782635769c3a39
smorkl <smsalazar0319@gmail.com>
2024-05-28 11:31:58 -0500

    create file

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def common_elements(set_1, set_2):
+    new_set = {""}
     for i in set_1:
         for x in set_2:
             if i == x:
                 return i
+    return new_set
