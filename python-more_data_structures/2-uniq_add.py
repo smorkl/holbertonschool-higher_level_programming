2a43104e3b7ed529f503c7db0bfe8e38f8d4fec5
smorkl <smsalazar0319@gmail.com>
2024-05-28 11:18:24 -0500

    create file

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,21 +1,4 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
-    """
-    Creates a new list containing only unique elements from the input list.
-
-    Args:
-        my_list (list, optional): The input list. Defaults to an empty list.
-
-    Returns:
-        list: A new list containing the unique elements.
-    """
-
-    seen = set()
-    new_list = []
-    for num in my_list:
-        if num not in seen:
-            seen.add(num)
-            new_list.append(num)
-    new_string = str(*new_list)
-    return new_string
+    return sum(set(my_list))
