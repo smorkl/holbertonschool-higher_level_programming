22b5e6afa37c804a1cf52a4a1c8544d6c5cad030
smorkl <smsalazar0319@gmail.com>
2024-05-28 00:36:42 -0500

    create

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -9,11 +9,3 @@ def print_matrix_integer(matrix=[[]]):
     """
     for row in matrix:
         print(*row)
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-print_matrix_integer(matrix)
