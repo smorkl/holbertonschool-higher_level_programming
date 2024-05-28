218f47bae9728a7d1fbd4c4335aff43056b834ab
smorkl <smsalazar0319@gmail.com>
2024-05-28 00:43:37 -0500

    create

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,11 +2,16 @@
 
 def print_matrix_integer(matrix=[[]]):
     """
-    Prints each element of a 2D integer matrix in a separate line.
+    Prints each row of a 2D integer matrix on a separate line, with 3 elements per row.
 
     Args:
         matrix: A 2D list of integers (optional, defaults to an empty list).
     """
     for row in matrix:
+        counter = 0
         for element in row:
-            print("{:d}".format(element))
+            print(element, end=" ")
+            counter += 1
+            if counter == 3:
+                counter = 0
+                print()
