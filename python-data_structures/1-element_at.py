4c03955d89f944a1e9720045dbfd31e743741d1e
smorkl <smsalazar0319@gmail.com>
2024-05-27 20:32:53 -0500

    create

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    
-    if idx < 0:
-        return "none"
-    elif idx > len(my_list):
+    """Returns the element at a given index in the list.
+
+    Args:
+        my_list: The list to access.
+        idx: The index of the element to retrieve.
+
+    Returns:
+        The element at the specified index, or "none"
+        if the index is out of bounds.
+    """
+    if idx < 0 or idx >= len(my_list):
         return "none"
     else:
-        print(my_list(idx))
+        return my_list[idx]
