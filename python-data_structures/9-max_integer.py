221fa56c747f8b274337bbdbcd15997ba6567851
smorkl <smsalazar0319@gmail.com>
2024-05-28 02:11:11 -0500

    create

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,7 +5,6 @@ def max_integer(my_list=[]):
         return "None"
     else:
         largest = my_list[0]
-        
         for num in my_list:
             if num > largest:
                 largest = num
