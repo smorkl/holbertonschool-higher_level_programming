c4fdce14e2ecc5e5b788e3c918e21670ee95c608
smorkl <smsalazar0319@gmail.com>
2024-05-28 12:15:59 -0500

    create file

--- python-more_data_structures/5-number_keys.py
+++ python-more_data_structures/5-number_keys.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
 
 def number_keys(a_dictionary):
+    """
+    Counts the number of keys in a dictionary.
+
+    Args:
+        a_dictionary (dict): The dictionary to count keys from.
+
+    Returns:
+        int: The number of keys in the dictionary.
+    """
     return len(a_dictionary)
