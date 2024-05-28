e3a72113cfe81e53af3f85a3c7cb32c12230ff3f
smorkl <smsalazar0319@gmail.com>
2024-05-28 13:10:24 -0500

    create file

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -6,5 +6,5 @@ def update_dictionary(a_dictionary, key, value):
     if existe_key is True:
         a_dictionary.update(new_key)
     else:
-        a_dictionary[key] = value
+        a_dictionary.update(new_key)
  
