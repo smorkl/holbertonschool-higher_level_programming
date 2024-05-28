d2bda8077c89886e23b25c119b8501814f3959f8
smorkl <smsalazar0319@gmail.com>
2024-05-28 13:17:21 -0500

    create file

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
+    """Actualiza el valor de una clave en un diccionario, agregándola si no existe.
+
+    Args:
+        a_dictionary (dict): El diccionario a actualizar.
+        key (hashable): La clave a actualizar o agregar.
+        value (any): El nuevo valor para la clave.
+    """
     if key in a_dictionary:
-        a_dictionary[key] = value  # Actualización directa para clave existente
+        a_dictionary[key] = value
         return a_dictionary
     else:
         a_dictionary[key] = value
