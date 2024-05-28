3ff5364cddcd4abf4e250be36dcf32cf80aac08d
smorkl <smsalazar0319@gmail.com>
2024-05-28 13:13:19 -0500

    create file

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 def update_dictionary(a_dictionary, key, value):
-    new_key = {key: value}
-    existe_key = new_key in a_dictionary
-    if existe_key is True:
-        a_dictionary.update(new_key)
+    if key in a_dictionary:
+        a_dictionary[key] = value
     else:
-        a_dictionary.update(new_key)
+        a_dictionary[key] = value
  
