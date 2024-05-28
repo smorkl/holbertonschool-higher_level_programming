c651bb1ecaba026c11d0899cccc5f3907ec667a1
smorkl <smsalazar0319@gmail.com>
2024-05-28 10:10:19 -0500

    create

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
+    """
+    Creates a new matrix containing the elements of the input matrix squared.
+
+    Args:
+        matrix (list, optional): The input matrix. Defaults to an empty list.
+
+    Returns:
+        A new list containing the squared elements.
+    """
+
     new_matrix = []
 
     new_matrix = []
