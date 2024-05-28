b1dbd56d1d7d998048cf98beeb26d40fb6241ed2
smorkl <smsalazar0319@gmail.com>
2024-05-28 13:16:10 -0500

    create file

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
 def update_dictionary(a_dictionary, key, value):
-    if key in sorted(a_dictionary):
-        a_dictionary[key] = value
+    if key in a_dictionary:
+        a_dictionary[key] = value  # Actualización directa para clave existente
+        return a_dictionary
     else:
         a_dictionary[key] = value
- 
+        return a_dictionary
