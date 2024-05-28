3080d05d13ef220c43db840fd872e4b5c66928fb
smorkl <smsalazar0319@gmail.com>
2024-05-28 12:11:05 -0500

    create file

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def number_keys(a_dictionary):
+    for value in a_dictionary():
+        for char in value:
+            if char == ":":
+                count += 1
+    return count
