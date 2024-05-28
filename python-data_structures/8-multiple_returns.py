a648ea43c7cbfd14b7651de88b6ba80453b8068a
smorkl <smsalazar0319@gmail.com>
2024-05-28 01:55:23 -0500

    create

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    tupla
+    if len(sentence) < 0:
+        tupla = ("None")
+        return tupla
+    else:
+        tupla = (len(sentence), sentence(0))
+        return tupla
