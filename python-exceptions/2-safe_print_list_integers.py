df0e192817bb6bafd38125ad65b52a41d0dbb80f
smorkl <smsalazar0319@gmail.com>
2024-05-29 13:52:52 -0500

    create

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    try:
+        count = 0
+        for i in my_list:
+            element = i
+            if int(element):
+                print("{:d}".format(element), end=" ")
+                count += 1
+            else:
+                continue
+    except (ValueError, TypeError):
+        return count
