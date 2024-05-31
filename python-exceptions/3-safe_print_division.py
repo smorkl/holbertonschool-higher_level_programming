3e50ea10b4ec6d8a22c2099f4f9d2e167d096ae3
smorkl <smsalazar0319@gmail.com>
2024-05-31 09:21:43 -0500

    create

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -3,7 +3,8 @@
 def safe_print_division(a, b):
     try:
         resultado = a / b
-    
+        return resultado
     except (TypeError, ValueError):
         return None
-    return resultado
+    finally:
+        return None
