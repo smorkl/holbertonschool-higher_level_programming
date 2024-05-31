6263522d95f2c52c44c139282ba3ce14ffc5e8dc
smorkl <smsalazar0319@gmail.com>
2024-05-31 14:18:05 -0500

    create

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,16 +1,34 @@
 #!/usr/bin/python3
+
 """Square Class
 
-This class represents a square emmpty
+This class represents a square shape.
 
+Currently, it's an empty class, meaning it doesn't
+ define any attributes or methods
+specific to squares. However, you can extend this class
+ to add functionalities
+related to squares, such as:
+
+- Calculating area (side_length * side_length)
+- Calculating perimeter (4 * side_length)
+- Checking if another object is also a square
+- Drawing a visual representation of the square
+ using ASCII art or other libraries
+
+By adding methods and attributes, you can make this
+ class more useful for working
+with squares in your Python programs.
 """
 
 
-class square:
+class Square:
     """
-    class enmpty
+    This docstring can be used to explain the purpose of the Square class
+    and its functionalities.
 
-    create firt class
-
+    For example, you could describe how to create a Square object and
+    what methods might be available in the future.
     """
-    pass
+
+    pass  # This line indicates an empty class body
