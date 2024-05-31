e16b3fe1a8053a01c4ffa8e4afa903870bb68bea
smorkl <smsalazar0319@gmail.com>
2024-05-31 14:06:31 -0500

    create

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -5,10 +5,18 @@ class Square:
     This class represents a square shape.
 
     Currently, it only defines a constructor that prints a simple string
-    representation of a square using curly braces `{}`. However, you
-    can extend this class to add functionalities specific to squares,
-    such as calculating area, perimeter, or drawing a more visually
-    appealing representation.
+    representation of a square using curly braces `{}`. However, you can extend
+    this class to add functionalities specific to squares, such as calculating
+    area, perimeter, or drawing a more visually appealing representation.
+
+    **Docstring checks:**
+
+    - To view the docstring of this class:
+        ```bash
+        python3 -c 'print(__import__("your_module_name").Square.__doc__)'
+        ```
+        Replace `your_module_name` with the actual name of the file
+        containing this class.
     """
 
     def __init__(self):
@@ -17,7 +25,18 @@ class Square:
 
         When creating a Square object, this method is automatically called
         and prints the string representation of the square (`{}`).
+
+        **Docstring checks:**
+
+        - There is currently no docstring available for the constructor.
+        ```bash
+        # This will not print anything as there's no __doc__ for __init__
+        python3 -c 'print(__import__("your_module_name")
+        .Square.__init__.__doc__)'
+        ```
+        Replace `your_module_name` with the actual module name.
         """
+
         a = '{'
         b = '}'
-        print("{}{}".format(a, b))
+        print("{}{}".format(a, b))  # Print the square representation
