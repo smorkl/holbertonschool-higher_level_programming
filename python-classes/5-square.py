21eb57992d103f91996441a8cc650ca7c8c893c5
smorkl <smsalazar0319@gmail.com>
2024-06-03 17:46:07 -0500

    create

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -77,3 +77,18 @@ class Square:
             for colum in range(self.size):
                 print(char, end="")
             print()
+        print()
+my_square = Square(3)
+my_square.my_print()
+
+print("--")
+
+my_square.size = 10
+my_square.my_print()
+
+print("--")
+
+my_square.size = 0
+my_square.my_print()
+
+print("--")
