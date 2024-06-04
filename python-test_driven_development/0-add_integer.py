b7d413a49350b7ccf768cbe577b10f8969fd2e1e
smorkl <smsalazar0319@gmail.com>
2024-06-03 23:41:06 -0500

    create

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -17,10 +17,10 @@ def add_integer(a, b=98):
         TypeError: If either a or b is not an integer.
     """
     if not (isinstance(a, int) or isinstance(a, float)):
-        raise TypeError("a must be an integer or a float")
+        raise TypeError("a must be an integer")
 
     if not (isinstance(b, int) or isinstance(b, float)):
-        raise TypeError("b must be an integer or a float")
+        raise TypeError("b must be an integer")
 
     else:
         a = round(int(a))
