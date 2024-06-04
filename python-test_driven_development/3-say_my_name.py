44f120a9412810f9458930f22da435165eecae8a
smorkl <smsalazar0319@gmail.com>
2024-06-04 09:53:10 -0500

    crate

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -10,4 +10,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(first_name, (str)) and not isinstance(last_name, (str)):
         raise TypeError("first_name must be a string or last_name must be a string")
     else:
-        print("My name is {} {}$".format(first_name, last_name))
+        print("My name is {} {}".format(first_name, last_name))
