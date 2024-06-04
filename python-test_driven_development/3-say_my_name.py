1dd4b5e9476fa3c465527bcec8577d1811963e1e
smorkl <smsalazar0319@gmail.com>
2024-06-04 09:49:43 -0500

    crate

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -5,12 +5,3 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string or last_name must be a string")
     else:
         print("My name is {} {}$".format(first_name, last_name))
-
-say_my_name("John", "Smith")
-say_my_name("Walter", "White")
-say_my_name("Bob")
-try:
-    say_my_name(12, "White")
-except Exception as e:
-    print(e)
-
