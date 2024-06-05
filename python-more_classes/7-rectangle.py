fd7789e81ac1ae6bc85c9ef450fc90d3be26f6da
smorkl <smsalazar0319@gmail.com>
2024-06-05 16:33:23 -0500

    create

--- python-more_classes/7-rectangle.py
+++ python-more_classes/7-rectangle.py
@@ -22,7 +22,7 @@ class Rectangle:
     """
     number_of_instances = 0
 
-    def __init__(self, width=0, height=0, print_symbol="#"):
+    def __init__(self, width=0, height=0, print_symbol=""):
         """
         Initializes a Rectangle object.
 
