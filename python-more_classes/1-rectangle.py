4df3b645a83472cf28e503e4e6c4d6cbf6113e2f
smorkl <smsalazar0319@gmail.com>
2024-06-05 00:47:40 -0500

    crate

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -7,7 +7,8 @@ Defines an empty Rectangle class.
 
 class Rectangle:
     """
-    An immutable Rectangle class representing a rectangle with a width and height.
+    An immutable Rectangle class representing
+    a rectangle with a width and height.
 
     Attributes:
         width (int, non-negative): The width of the rectangle. Must be a
@@ -20,23 +21,43 @@ class Rectangle:
         TypeError: If the width is not an integer.
     """
 
-    def __init__ (self, width=0, height=0):
+    def __init__(self, width=0, height=0):
         """
         Initializes a Rectangle object.
 
+        This method is called automatically when you create a new
+        `Rectangle` object.
+        It sets the initial width and height of the rectangle.
+
         Args:
-            width (int, optional): The initial width of the rectangle. Defaults to 0.
-            height (int, optional): The initial height of the rectangle. Defaults to 0.
+            width (int, optional): The initial width of the rectangle.
+              Defaults to 0.
+            height (int, optional): The initial height of the rectangle.
+              Defaults to 0.
+
+        Raises:
+            ValueError: If either width or height is less than zero.
+            TypeError: If either width or height is not an integer.
         """
-        self._width = width
-        self._height = height
-    
+        try:
+            if height < 0:
+                raise ValueError("height must be >= 0")
+            self._height = height
+        except TypeError:
+            raise TypeError("height must be an integer")
+        try:
+            if width < 0:
+                raise ValueError("width must be >= 0")
+            self._width = width
+        except TypeError:
+            raise TypeError("width must be an integer")
+
     def get_width(self):
         """
         return the valor of width
         """
         return self._width
-    
+
     def set_width(self, value):
         """
         In this function we handle two kinds
@@ -48,13 +69,13 @@ class Rectangle:
             self._width = value
         except TypeError:
             raise TypeError("width must be an integer")
-    
+
     def get_height(self):
         """
         return the valor of return height
         """
         return self._height
-    
+
     def set_height(self, value):
         """
         In this function we handle two kinds
