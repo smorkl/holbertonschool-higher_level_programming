cc40efd3804bc20a1197c43d9b83b0192aa897df
smorkl <smsalazar0319@gmail.com>
2024-06-06 17:07:58 -0500

    create

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def lookup(obj):
+    """Returns a dictionary containing the attributes and methods of an object.
+
+    Args:
+        obj: The object to be inspected.
+
+    Returns:
+        A dictionary where keys are attribute or method names and values are
+        their types (str for attributes, function for methods).
+    """
+    result = {}
+    for name in dir(obj):
+        value = getattr(obj, name)
+        if callable(value):
+            result[name] = "function"
+        else:
+            result[name] = type(value).__name__
+    return result
