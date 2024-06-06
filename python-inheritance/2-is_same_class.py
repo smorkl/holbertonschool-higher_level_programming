8339668026a31b05e9217d6a335e416570881072
smorkl <smsalazar0319@gmail.com>
2024-06-06 18:59:36 -0500

    create

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -8,5 +8,7 @@ def is_same_class(obj, a_class):
     """
     if the object is an instance of the class returns true otherwise false
     """
-    answer = (isinstance(obj, a_class))
-    return answer
+    if (isinstance(obj, a_class)):
+        return True
+    else:
+        return False
