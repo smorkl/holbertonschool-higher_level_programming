e01ec9f1297c8cf197249c7f21a93340f9f057f4
smorkl <smsalazar0319@gmail.com>
2024-06-06 17:13:15 -0500

    create

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -14,11 +14,4 @@ def lookup(obj):
         A dictionary where keys are attribute or method names and values are
         their types (str for attributes, function for methods).
     """
-    result = {}
-    for name in dir(obj):
-        value = getattr(obj, name)
-        if callable(value):
-            result[name] = "function"
-        else:
-            result[name] = type(value).__name__
-    return result
+    return dir(obj)
