79a6da7e8b7113fedf672145faabd46a9cb88ee7
smorkl <smsalazar0319@gmail.com>
2024-06-06 20:09:35 -0500

    create

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -19,7 +19,7 @@ class BaseGeometry():
             raise (TypeError("<name> must be an integer"))
         if value <= 0:
             raise (ValueError("<name> must be greater than 0"))
-    
+
     def area(self):
         """
         this fuctio returns an exception error
