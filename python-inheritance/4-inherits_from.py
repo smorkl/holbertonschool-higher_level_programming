855896a8be8e0ebc025e48df0664704030f95e0c
smorkl <smsalazar0319@gmail.com>
2024-06-06 19:20:01 -0500

    create

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,7 +6,9 @@ function determines whether an object was inherited or not
 
 def inherits_from(obj, a_class):
     """
-    if the object is an instance of the class returns true otherwise false
+    The function returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the
+    specified class; otherwise False.
     """
     if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
