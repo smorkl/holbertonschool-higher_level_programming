10b38c1cb679ab69726e4f73bb1575fd17d56815
smorkl <smsalazar0319@gmail.com>
2024-06-06 19:44:52 -0500

    create

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+empty class
+"""
+
+
+class BaseGeometry():
+    """
+    empty class BaseGeometry
+    """
+    def area(self):
+        raise(Exception("area() is not implemented"))
+        pass
