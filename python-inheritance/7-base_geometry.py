16266bfb6d0a869fe2fee0a471b4882c07e042b0
smorkl <smsalazar0319@gmail.com>
2024-06-06 20:11:01 -0500

    create

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -16,9 +16,9 @@ class BaseGeometry():
             TypeError and ValueError
         """
         if type(value) is not int:
-            raise (TypeError("<name> must be an integer"))
+            raise (TypeError("name must be an integer"))
         if value <= 0:
-            raise (ValueError("<name> must be greater than 0"))
+            raise (ValueError("name must be greater than 0"))
 
     def area(self):
         """
