84a2dfdc7bcf51c4ce82412602ebf89e132f1650
smorkl <smsalazar0319@gmail.com>
2024-06-06 21:41:52 -0500

    create

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+Rectangle that inherits from BaseGeometry 
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """
+    Representa un rectángulo con atributos de ancho y alto.
+    """
+    def __init__(self, width, height):
+        """
+        Inicializa un objeto Rectangle.
+
+        Args:
+            width (int): El ancho del rectángulo.
+            height (int): La altura del rectángulo.
+
+        Raises:
+            TypeError: Si width o height no es un entero.
+            ValueError: Si width o height no son estrictamente positivos.
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self._width = width
+        self.height = height
+    
+    def __str__(self):
+        return "[Square] <width>/<height>"
+    
+    def area(self):
+        result = (self.__width * self.__height)
+        return result
