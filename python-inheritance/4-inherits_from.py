4c18e55869401bb449b5172a6275c85c81d40f8c
smorkl <smsalazar0319@gmail.com>
2024-06-06 19:17:59 -0500

    create

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -8,9 +8,7 @@ def inherits_from(obj, a_class):
     """
     if the object is an instance of the class returns true otherwise false
     """
-    if issubclass(type(obj), a_class):
-        return True
-    elif(isinstance(obj, a_class)):
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
         return True
     else:
         return False
