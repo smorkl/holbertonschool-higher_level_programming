d6717d685cd648e0709f1f4af14c97739efed89d
smorkl <smsalazar0319@gmail.com>
2024-06-09 11:46:12 -0500

    create

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -5,29 +5,17 @@ class VerboseList
 
 
 class VerboseList(list):
+    """class verboselist
     """
-    A class that inherits from list and provides verbose
-    messages on modifications.
-    """
-
-    def __init__(self, iterable=None):
-        """
-        Initializes the VerboseList object.
-
-        Args:
-            iterable: An optional iterable to populate the list initially.
-        """
-        super().__init__(iterable)
-
-    def append(self, value):
+    def append(self, item):
         """
         Appends an element to the list and prints a message.
 
         Args:
-            value: The element to be appended.
+            item: The element to be appended.
         """
-        super().append(value)
-        print(f"Added [{value}] to the list")
+        super().append(item )
+        print(f"Added [{item}] to the list")
 
     def extend(self, iterable):
         """
@@ -40,20 +28,20 @@ class VerboseList(list):
         super().extend(iterable)
         print(f"Extended the list with [{num_added}] items.")
 
-    def remove(self, value):
+    def remove(self, item):
         """
         Removes the first item from the list with the specified
-        value and prints a message.
+        item and prints a message.
 
         Args:
-            value: The value of the element to be removed.
+            item: The item of the element to be removed.
 
         Raises:
-            ValueError: If the value is not found in the list.
+            itemError: If the item is not found in the list.
         """
-        if value in self:
-            super().remove(value)
-            print(f"Removed [{value}] from the list.")
+        if item in self:
+            super().remove(item)
+            print(f"Removed [{item}] from the list.")
 
     def pop(self, index=-1):
         removed_element = super().pop(index)
