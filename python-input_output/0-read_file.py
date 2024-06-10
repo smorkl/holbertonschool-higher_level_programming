e969f413d1ff8210f4331939d160080cd5fa4e79
smorkl <smsalazar0319@gmail.com>
2024-06-10 12:55:50 -0500

    create

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
-"""
+"""read file
 """
 
 
 def read_file(filename=""):
-    with open(filename):
-        pass
+    """
+    Reads the content of a file and returns it as a string.
+
+    Args:
+        filename (str, optional): The filename to open.
+        Defaults to "".
+
+    Returns:
+        str: The contents of the file as a string, or an empty
+        string if the file is empty or cannot be opened.
+    """
+    with open(filename, 'r') as file:
+        contents = file.read()
+        return contents
