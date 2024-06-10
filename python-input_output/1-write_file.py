2bc658607e7cd7174c5836c75bbdad9714a8aa7d
smorkl <smsalazar0319@gmail.com>
2024-06-10 13:20:46 -0500

    create

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -8,4 +8,5 @@ def write_file(filename="", text=""):
     Writes the provided text to the specified file.
     """
     with open(filename, 'w', encoding="utf-8") as file:
-        return len(text.encode('utf-8'))
+        write = file.weite(text)
+    return write
