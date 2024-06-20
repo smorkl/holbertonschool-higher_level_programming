bd4ec4535738dda32f0b24568542d5bfee4baa3c
smorkl <smsalazar0319@gmail.com>
2024-06-19 22:05:45 -0500

    create

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,39 +6,16 @@ class_to_json
 
 def class_to_json(obj):
     """
-    Converts an object into a JSON-serializable dictionary.
+    Converts an object's attributes into a JSON-serializable dictionary.
+
+    This function returns the `__dict__` attribute of the given object,
+    which contains all the attributes of the object that are defined
+    in the object's `__init__` method and are JSON-serializable.
 
     Args:
-        obj: An instance of a class with serializable attributes.
+        obj: An instance of a class.
 
     Returns:
-        dict: A dictionary containing JSON-serializable
-        attributes of the object.
+        dict: A dictionary containing all attributes of the object.
     """
-    attributes = vars(obj)
-    json_dict = {}
-
-    for key, value in private_attributes.items():
-        if isinstance(value, list):
-            json_dict[key] = [item for item in value if _is_json_serializable(item)]
-        elif isinstance(value, dict):
-            json_dict[key] = {k: v for k, v in value.items() if _is_json_serializable(v)}
-        elif isinstance(value, (str, int, bool)):
-            json_dict[key] = value
-        else:
-            json_dict[key] = repr(value)    
-    return json_dict
-
-
-def _is_json_serializable(value):
-    """
-    Helper function to check if a value is JSON-serializable.
-
-    Args:
-        value: The value to check.
-
-    Returns:
-        bool: True if the value is JSON-serializable, False otherwise.
-
-    """
-    return isinstance(value, (str, int, bool, list, dict))
+    return (obj.__dict__)
