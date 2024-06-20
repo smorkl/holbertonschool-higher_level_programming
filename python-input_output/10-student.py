5ad3b216187af25655d7367e8eb9b225e977d8af
smorkl <smsalazar0319@gmail.com>
2024-06-19 22:37:33 -0500

    create

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -5,12 +5,29 @@ class students
 
 
 class Student:
+    """
+    Initializes a new Student object.
+
+    Args:
+        first_name (str): The student's first name.
+        last_name (str): The student's last name.
+        age (int): The student's age.
+    """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        """
+        Helper function to filter attributes based on the provided list.
+
+        Args:
+            attrs (list): A list of attribute names to include.
+
+        Returns:
+            dict: A dictionary containing the filtered attributes.
+        """
         if isinstance(attrs, list)
             return {attrs: getattr(self, attrs) for attrs in attr
                     if hasattr(self, attrs)}
