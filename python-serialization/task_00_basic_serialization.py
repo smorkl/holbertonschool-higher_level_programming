c86ccb845345fbe45f29ae07116fca0bf2fe34ad
smorkl <smsalazar0319@gmail.com>
2024-06-21 09:34:52 -0500

    create

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,9 +1,34 @@
+#!/usr/bin/python3
+"""
+serialize and save to file
+and load and deserialize
+"""
 import json
 
+
 def serialize_and_save_to_file(data, filename):
-    with open(f"{filename}", "w") as write_file:
+    """
+    Serializes data in JSON format and saves it to a file.
+
+    Arguments:
+    data: The data to be serialized.
+    filename (str): The name of the file
+    where the data will be saved.
+    """
+    with open(filename, "w") as write_file:
         json.dump(data, write_file)
 
+
 def load_and_deserialize(filename):
-    with open(f"{filename}", "r") as read_file:
+    """
+    Loads data from a file and deserializes it from JSON format.
+
+    Args:
+    filename (str): The name of the file from which the data will be loaded.
+
+    Returns:
+    The data deserialized from the JSON file.
+    """
+    with open(filename, "r") as read_file:
         data = json.load(read_file)
+    return data
