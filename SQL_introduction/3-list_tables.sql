adef4e45dfebb46203f57656420b081f7569d043
sek2 <sek2@sebask2>
2024-06-25 23:08:50 -0500

    create

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,2 +1,8 @@
---Write a script that lists all the tables of a database in your MySQL server.
-SHOW TABLES;
+-- Script to list all tables of a specified database
+
+-- Check if the user provided a database name argument
+-- You need to replace the variable `@database_name` with the actual database name passed as an argument
+
+SET @database_name = 'Tables_in_mysql'; 
+-- Generate the SQL command to show all tables in the specified database
+SELECT CONCAT('SHOW TABLES IN `', @database_name, '`;') AS sql_command;
