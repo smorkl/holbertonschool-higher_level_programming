6873dbd2272b8e1d49c63b29a89d7358ed6faf0e
sek2 <sek2@sebask2>
2024-06-25 23:29:25 -0500

    create

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,4 @@
 --script that creates a table called first_table in the current database in your MySQL server
-USE hbtn_test_db_4;
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
