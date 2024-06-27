e2f1699a1d25b1382a54c2180db5036bb1ca5f1d
sek2 <sek2@sebask2>
2024-06-26 22:34:04 -0500

    create

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,5 +1,4 @@
-from flask import Flask
-from flask import jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -32,6 +31,32 @@ def get_user(username):
         return jsonify(users[username])
     else:
         return jsonify({"error": "User not found"}), 404
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    """
+    add a new user to the userdata based on the POST request data.
+    """
+    if request.method == "POST":
+        try:
+            user_data = request.get_json()
+        except (KeyError, TypeError):
+            return jsonify({"error": "Invalid JSON data provided"}), 400
+
+        username = user_data.get("username")
+        name = user_data.get("name")
+        age = user_data.get("age")
+        city = user_data.get("city")
+    
+        if not all([username, name]):
+            return jsonify({"error": "Missing required fields (username and name)"}), 400
+
+        users[username] = {"name": name, "age": age, "city": city}
+
+        return jsonify({"message": "User added successfully", "user": users[username]})
+
+    return jsonify({"error": "Method not allowed"}), 405
+
 
 if __name__ == "__main__":
     app.run()
