e28f4a9a30f642366a0b889768d684eb84da0f65
smorkl <smsalazar0319@gmail.com>
2024-09-07 11:23:59 -0500

    create

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    """Imprime los numeros del 1 al 100, siguiendo las reglas de FizzBuzz, separados por espacios.
+
+    """
+    resultado = ""  # Inicializamos la variable resultado
+    for numero in range(1, 101):
+        if numero % 15 == 0:
+            resultado += "FizzBuzz "
+        elif numero % 3 == 0:
+            resultado += "Fizz "
+        elif numero % 5 == 0:
+            resultado += "Buzz "
+        else:
+            resultado += str(numero) + " "
+
+    # Eliminamos el último espacio extra
+    resultado = resultado.rstrip()
+    print(resultado)
+
+# Llamamos a la función para ejecutar el código
+fizzbuzz()
