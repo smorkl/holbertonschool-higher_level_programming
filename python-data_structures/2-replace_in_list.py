21d20112d7b97eff56e6291c744fd515b1f13192
smorkl <smsalazar0319@gmail.com>
2024-09-14 14:39:28 -0500

    create

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -14,7 +14,7 @@ def replace_in_list(my_list, idx, element):
         The modified list, or return list origin.
     """
 
-    if idx < 0 or idx >= len(my_list) + 1:
+    if idx < 0 or idx > len(my_list):
         return my_list
     else:
         my_list[idx] = element
