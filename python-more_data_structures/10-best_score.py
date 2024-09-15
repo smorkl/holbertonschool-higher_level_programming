3cc773e3c1f8c744c8c507eea4e8289ae7cf3fef
smorkl <smsalazar0319@gmail.com>
2024-09-14 23:13:32 -0500

    create

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,8 +4,11 @@ def best_score(a_dictionary):
     # Iterando sobre los valores
     best_value = 0
     best_key = None
-    for key, value in a_dictionary.items():
-        if value > best_value:
-            best_key = key
-            best_value = value
-    return best_key
+    if len(a_dictionary) == 0:
+        return None
+    else:
+        for key, value in a_dictionary.items():
+            if value > best_value:
+                best_key = key
+                best_value = value
+        return best_key
