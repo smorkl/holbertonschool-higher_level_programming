065f83e321e4d843f5446f6add32528aba2fedc6
smorkl <smsalazar0319@gmail.com>
2024-09-14 21:23:10 -0500

    modifique

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) == 1 and len(tuple_b) == 1:
         tupla_c = (tuple_a[0] + tuple_b[0], 0)
@@ -19,5 +20,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
         return tupla_c
     else:
         tupla_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-        return tupla_c 
-    
+        return tupla_c
