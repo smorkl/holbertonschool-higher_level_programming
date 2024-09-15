28eb4507e387cf87c772a1b1e37d5c9adbcddfbe
smorkl <smsalazar0319@gmail.com>
2024-09-14 23:07:52 -0500

    create

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-
 def best_score(a_dictionary):
     # Iterando sobre los valores
     best_value = 0
@@ -9,6 +8,4 @@ def best_score(a_dictionary):
         if value > best_value:
             best_key = key
             best_value = value
-
-
-a_dictionary = {"John": 12, "Bob": 14, "Mike": 14, "Molly": 16, "Adam": 10}
+    return (best_key, best_value)
