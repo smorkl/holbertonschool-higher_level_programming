70833a4d79fa182b8042cf8437d65259f94f5c9e
smorkl <smsalazar0319@gmail.com>
2024-09-19 16:43:44 -0500

    create

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,12 +1,34 @@
 #!/usr/bin/python3
 """
+this module defines the 'Square' class, which represents a square.
 """
 
 
 class Square:
-    """ """
+    """
+    Represents a square.
+
+    Attributes:
+        __size (int): The side length of the square.
+
+    Args:
+        size (int, optional): The initial side length
+        of the square. Defaults to 0.
+    """
 
     def __init__(self, size=0) -> None:
+        """
+        Initializes a new Square object.
+
+        Args:
+            size (int, optional): The initial side
+            length of the square. Must be a non-negative
+            integer. Defaults to 0.
+
+        Raises:
+            TypeError: If `size` is not an integer.
+            ValueError: If `size` is negative.
+        """
 
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
