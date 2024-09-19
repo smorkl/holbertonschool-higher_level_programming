81f75d2e9ff16f706b9a0c2e28709a571cb947e7
smorkl <smsalazar0319@gmail.com>
2024-09-19 09:37:36 -0500

    create

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,34 +1,7 @@
 #!/usr/bin/python3
-
-"""Square Class
-
-This class represents a square shape.
-
-Currently, it's an empty class, meaning it doesn't
- define any attributes or methods
-specific to squares. However, you can extend this class
- to add functionalities
-related to squares, such as:
-
-- Calculating area (side_length * side_length)
-- Calculating perimeter (4 * side_length)
-- Checking if another object is also a square
-- Drawing a visual representation of the square
- using ASCII art or other libraries
-
-By adding methods and attributes, you can make this
- class more useful for working
-with squares in your Python programs.
 """
-
-
+This class is empty
+"""
 class Square:
-    """
-    This docstring can be used to explain the purpose of the Square class
-    and its functionalities.
+    pass
 
-    For example, you could describe how to create a Square object and
-    what methods might be available in the future.
-    """
-
-    pass  # This line indicates an empty class body
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,39 +1,19 @@
 #!/usr/bin/python3
-"""Square Class
-
-square class
-
 """
+Represents a square.
 
+Attributes:
+__size (int): The side length of the square.
 
-class Square:
-    """
-    This class represents a square shape.
-
-    Currently, it defines a constructor (`__init__`)that takes
-    a single argument called `size` and assigns it to a private
-    attribute named `__size`. Private attributes in Python follow
-    the convention of being prefixed with double underscores (`__`).
-
-    While not truly private (they can still be accessed indirectly), this
-    convention discourages direct access and promotes encapsulation, where
-    internal data is protected and accessed through methods.
-    """
-
+Args:
+size (int): The initial side length of the square.
+"""
+class square:
     def __init__(self, size):
         """
-        The constructor of the Square class.
+        Initializes a new Square object.
 
-        This method is called automatically whenever
-        you create a new Square object. It takes one argument:
-
-        - size (int): The size of the square. This could
-        represent the length of a side of the square.
-
-        The constructor assigns the provided `size` to a
-        private attribute named `__size`. It doesn't perform
-        any additional calculations or checks in this basic
-        implementation.
+        Args:
+            size (int): The side length of the square.
         """
-
         self.__size = size
