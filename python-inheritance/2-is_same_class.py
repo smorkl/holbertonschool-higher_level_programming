b2c5dfa15a9b84f8b201cd9ff66f864e43ca653d
Briana1984 <brian.a.03@hotmail.com>
2024-09-25 11:24:10 -0500

    modification

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -7,7 +7,7 @@ instance of the specified class ; otherwise False
 
 def is_same_class(obj, a_class):
     """
-    if the object is an instance of the class 
+    if the object is an instance of the class
     returns true otherwise false
     """
     if type(obj) is a_class:
