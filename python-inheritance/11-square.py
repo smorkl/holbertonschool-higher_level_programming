258784fe1b8e53ace8fe889fe68234c8ac45ce76
smorkl <smsalazar0319@gmail.com>
2024-09-27 01:16:41 -0500

    create

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -2,13 +2,14 @@
 """
 square that inherits from rectangle
 """
-Rectangle = __import__('9-rectangle').Rectangle
+Rectangle = __import__("9-rectangle").Rectangle
 
 
 class Square(Rectangle):
     """
     Represents a square with size attributes.
     """
+
     def __init__(self, size):
         """
         Initializes a square object.
@@ -20,12 +21,12 @@ class Square(Rectangle):
         TypeError: If size is  not an integer.
         ValueError: If size is not strictly positive.
         """
-        super().__init__(self.integer_validator)
-        self.integer_validator("size", size)
+        super().integer_validator("size", size)
         self._size = size
+
     def __str__(self):
         return f"[Square] {self._size}/{self._size}"
-    
+
     def area(self):
-        result = (self._size * 2)
+        result = self._size ** 2
         return result
