ca977de0f636999d2ca3ad710dcddd0e4f715ccf
smorkl <smsalazar0319@gmail.com>
2024-09-28 18:19:51 -0500

    modifique

--- python-abc/task_04_flyingfish.py
+++ python-abc/task_04_flyingfish.py
@@ -1,12 +1,29 @@
+from abc import ABC, abstractmethod
 
-class CountedIterator():
-    def __init__(self, iterable) -> None:
-        self.iterator = iter(iterable)
-        self.count = 0
+class Fish(ABC):
+    @abstractmethod
+    def swim(self):
+        pass
 
-    def get_count(self):
-        return self.count
-    
-    def __next__(self):
-        self.count += 1
-        return next(self.iterator)
+    @abstractmethod
+    def habitat(self):
+        pass
+
+class Bird(ABC):
+    @abstractmethod
+    def fly(self):
+        pass
+
+    @abstractmethod
+    def habitat(self):
+        pass
+
+class FlyingFish(Fish, Bird):
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives both in water and the sky!")
