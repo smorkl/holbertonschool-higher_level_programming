17eb26f5a9961ff1863ddd3475ddc276412aa4c6
smorkl <smsalazar0319@gmail.com>
2024-09-28 18:27:27 -0500

    modifique

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -11,5 +11,11 @@ class FlyMixin(ABC):
         pass
 
 class Dragon(SwimMixin, FlyMixin):
+    def swim(self):
+        print("The dragon glides through the water!")
+
+    def fly(self):
+        print("The dragon soars through the sky!")
+
     def roar(self):
         print("The dragon roars!")
