537fdb7c52c888ee674e098e2fcb90a99279e06a
smorkl <smsalazar0319@gmail.com>
2024-09-28 15:54:02 -0500

    modifique

--- python-abc/task_00_abc.py
+++ python-abc/task_00_abc.py
@@ -1,23 +1,26 @@
+from abc import ABC, abstractmethod
+
 """
 class of Animal
 """
-
-from abc import ABC, abstractmethod
-
-
 class Animal(ABC):
 
     @abstractmethod
     def sound(self):
         pass
 
-
+"""
+class Dog 
+"""
 class Dog(Animal):
 
     def sound(self):
         return "Bark"
 
 
+"""
+class Cat
+"""
 class Cat(Animal):
 
     def sound(self):
