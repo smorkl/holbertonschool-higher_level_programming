b94a3db31c1bd218ced1c657353e2fec7d934678
Briana1984 <brian.a.03@hotmail.com>
2024-10-23 12:20:27 -0500

    create

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -8,38 +8,47 @@ def fetch_and_print_posts():
     """
 
     url = "https://jsonplaceholder.typicode.com/posts"
-
+    
+    #requests get
     response = requests.get(url)
+    #print status
     print(f"Status Code: {response.status_code}")
 
-    posts = response.json()
-    
-    for post in posts:
-        print(f"{post['title']}")
+    if response.status_code == 200:
+        posts = response.json()
+        
+        for post in posts:
+            print(f"{post['title']}")
+    else:
+        print("Failed to fetch posts.")
 
 def fetch_and_save_posts():
-    """
-    Fetches all posts from JSONPlaceholder and saves them to a CSV file.
-    """
-
-    url = "https://jsonplaceholder.typicode.com/posts"
-
-    response = requests.get(url)
-
-    posts = response.json()
-
-    # Structure data into list of dictionaries
-    formatted_posts = []
-    for post in posts:
-        formatted_posts.append({
-            "id": post["id"],
-            "title": post["title"],
-            "body": post["body"]
-        })
-
-        # Write data to CSV file
-    with open("posts.csv", "w", newline="") as csvfile:
-        fieldnames = ["id", "title", "body"]
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
-        writer.writerows(formatted_posts)
+        """
+        """
+        url = "https://jsonplaceholder.typicode.com/posts"
+        
+        #request get 
+        response = requests.get(url)
+        
+        if response.status_code == 200:
+            """
+            
+            """
+            #convert input to json
+            posts = response.json()
+            
+            # Structure data into list of dictionaries
+            formatted_posts = []
+            for post in posts:
+                formatted_posts.append({
+                    "id": post["id"],
+                    "title": post["title"],
+                    "body": post["body"]
+                })
+                
+            # Write data to CSV file
+            with open("posts.csv", "w", newline="") as csvfile:
+                fieldnames = ["id", "title", "body"]
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerows(formatted_posts)
