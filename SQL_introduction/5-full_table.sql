a401ed3a31773bd8f9fcff5ab3bbb2480d649c0e
smorkl <smsalazar0319@gmail.com>
2024-11-07 12:38:58 -0500

    create

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,2 +1,4 @@
 --a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server --
-SHOW CREATE TABLE first_table;
+SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
