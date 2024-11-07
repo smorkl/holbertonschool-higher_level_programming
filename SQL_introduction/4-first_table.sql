fe51a2d44ffe4b95b054fd0a8941a4b32aec6d6d
smorkl <smsalazar0319@gmail.com>
2024-11-07 12:35:51 -0500

    create

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 --script that creates a table called first_table in the current database in your MySQL server--
 CREATE TABLE IF NOT EXISTS first_table (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
+    id INT,
+    name VARCHAR(256)
 );
