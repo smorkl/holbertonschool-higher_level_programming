342fabf130542fdc0fde4b8d9144bffd0334150b
smorkl <smsalazar0319@gmail.com>
2024-11-07 12:40:30 -0500

    create

--- SQL_introduction/5-full_table.sql
+++ SQL_introduction/5-full_table.sql
@@ -1,4 +1,22 @@
 --a script that prints the following description of the table first_table from the database hbtn_0c_0 in your MySQL server --
-SELECT COLUMN_NAME, DATA_TYPE, CHARACTER_MAXIMUM_LENGTH
-FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE() AND TABLE_NAME = 'first_table';
+SELECT 
+    CONCAT(
+        'CREATE TABLE `', table_name, '` (',
+        GROUP_CONCAT(
+            CONCAT(
+                '`', column_name, '` ', column_type,
+                CASE 
+                    WHEN is_nullable = 'NO' THEN ' NOT NULL'
+                    ELSE ''
+                END,
+                CASE 
+                    WHEN column_default IS NOT NULL THEN ' DEFAULT ' 
+                        CONCAT('\'', column_default, '\'')
+                    ELSE ''
+                END
+            ) SEPARATOR ', '
+        ),
+        ', PRIMARY KEY (`id`) ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci'
+    ) AS create_statement
+FROM information_schema.columns
+WHERE table_schema = DATABASE() AND table_name = 'first_table';
