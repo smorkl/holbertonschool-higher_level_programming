f13c7188f595e4bf07dea0afc6d9072618d93457
smorkl <smsalazar0319@gmail.com>
2024-11-07 13:12:30 -0500

    create

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- this script creates a new table named second_table  --
 
 CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table  VALUES(1, 'John', 10)
-INSERT INTO second_table  VALUES(2, 'Alex', 3)
-INSERT INTO second_table  VALUES(3, 'Bob', 14)
-INSERT INTO second_name values(4, 'George', 8)
+INSERT INTO second_table  VALUES(1, 'John', 10);
+INSERT INTO second_table  VALUES(2, 'Alex', 3);
+INSERT INTO second_table  VALUES(3, 'Bob', 14);
+INSERT INTO second_table values(4, 'George', 8);
