249dd4a7d566490bafaf169c933ff2133a453ec0
smorkl <smsalazar0319@gmail.com>
2024-11-07 17:27:07 -0500

    create

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,3 +1,3 @@
 -- This script selects from the second_table table the columns score name and arranges them in order of order and that meet the requirement that the score is less than or equal to 10 --
 
-SELECT score, name FROM second_table ORDER BY score >= 10 DESC;
+SELECT score, name FROM second_table ORDER BY score >= 10 DESC OR name DESC;
