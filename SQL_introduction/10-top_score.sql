61db6963f29c9af0e4f6cff2d4b43c2b14208fbe
smorkl <smsalazar0319@gmail.com>
2024-11-07 13:32:17 -0500

    create

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,3 +1,4 @@
 -- This script selects from the second_table table the columns score name and sorts them in decreasing order of score. -- 
 
-SELECT second_table(score, name) ORDER BY score;
+
+SELECT score, name FROM second_table ORDER BY score;
