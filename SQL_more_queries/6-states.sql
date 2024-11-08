1e23260b71a83504635073d72f5f0e32ecffdf5f
smorkl <smsalazar0319@gmail.com>
2024-11-08 11:21:59 -0500

    create

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,7 +1,8 @@
 -- This script sets up a database called hbtn_0d_usa with a state table that has two columns: id, which is a unique auto-incrementing primary key, and name, which stores a state name
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(
+use hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256)
 )
