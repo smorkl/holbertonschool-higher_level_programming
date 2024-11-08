c2e8c1162a117fd6a06898bc582670709b3653a1
smorkl <smsalazar0319@gmail.com>
2024-11-08 12:16:08 -0500

    create

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,10 +1,9 @@
 -- This script creates a database and an associated table with variables that meet certain requirements id INT (starts at 1, primary key), state_id INT (not NULL), name VARCHAR (256), and the key state_id is associated with the table state_id
 
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
-USE hbtn_0d_usa; 
-CREATE TABLE IF NOT EXISTS cities (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
-    name VARCHAR(256),
-    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
+    name VARCHAR(256) NOT NULL
 )
