554bc5f1b4707cee0eac4f2918ed839deea0df41
smorkl <smsalazar0319@gmail.com>
2024-11-08 11:10:42 -0500

    create

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
--- --
+-- The script creates the table unique_id on the MySQL server with two variables id INT and name VARCHAR, ensuring that the value of id is unique --
 
 CREATE TABLE IF NOT EXISTS unique_id (
     id INT UNIQUE DEFAULT 1,
