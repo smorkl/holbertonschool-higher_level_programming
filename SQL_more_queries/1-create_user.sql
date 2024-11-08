e266708c1826fbef800b5279e37677056e79e3cf
smorkl <smsalazar0319@gmail.com>
2024-11-08 10:01:32 -0500

    create

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- the script create new user in localhost with  password--
 
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-FLUSH PRIVILEGES;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
