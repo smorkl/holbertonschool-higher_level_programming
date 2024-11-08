470db256cd8db097f6551dc883e95abcc2b3af29
smorkl <smsalazar0319@gmail.com>
2024-11-08 09:54:04 -0500

    create

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,4 +2,3 @@
 
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_1'@'localhost' | grep -v 'WITH GRANT OPTION';
