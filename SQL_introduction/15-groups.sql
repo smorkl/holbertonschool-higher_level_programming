b05bf90992b45230018a3604cba9426f7880c0cf
smorkl <smsalazar0319@gmail.com>
2024-11-07 20:25:12 -0500

    create

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,24 +1,6 @@
 -- sndndi --
 
-DELIMITER $$
-CREATE PROCEDURE mi_procedimiento()
-BEGIN
-  DECLARE done INT DEFAULT FALSE;
-  DECLARE cur CURSOR FOR SELECT DISTINCT score FROM second_table;
-  DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
-  DECLARE @score INT;
-
-  OPEN cur;
-
-  read_loop: LOOP
-    FETCH cur INTO @score;
-    IF done THEN
-      LEAVE read_loop;
-    END IF;
-
-    INSERT INTO mi_tabla (valor) VALUES (@score);
-  END LOOP;
-
-  CLOSE cur;
-END $$
-DELIMITER ;
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
