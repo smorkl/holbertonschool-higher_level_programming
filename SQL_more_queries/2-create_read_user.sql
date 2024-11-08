1a59b748ef3080412d0f3fd64ef0dc72cf01f3c5
smorkl <smsalazar0319@gmail.com>
2024-11-08 10:11:49 -0500

    create

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- the script creates database hbtn_0d_2 and user user_0d_2 and grants it only select privilege --
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_2_pwd';
+GRANT SELECT RELOAD ON *.* TO 'user_0d_2'@'localhost';
