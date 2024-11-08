35551bb1f349aa12f9f8ea914b983c3f09470d54
smorkl <smsalazar0319@gmail.com>
2024-11-08 07:26:16 -0500

    create

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
--- --
+-- This script will list all records in the second_table table of the hbtn_0c_0 database on your MySQL server and it has to be only the ones that match rows with value --
 
 SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
