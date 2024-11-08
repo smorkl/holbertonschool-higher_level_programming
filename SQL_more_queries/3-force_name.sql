da4f826ca49c69c3b9f05ff6ec2b18f71811f131
smorkl <smsalazar0319@gmail.com>
2024-11-08 10:50:52 -0500

    create

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,6 +1,6 @@
 -- The script creates the table force_name on the MySQL server --
 
 CREATE TABLE IF NOT EXISTS force_name (
-    id int,
-    name varchar(255),
+    id INT,
+    name VARCHAR(255)
 );
