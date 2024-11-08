0a14bb55603296993f38b216388cd2b3c4e9c6c9
smorkl <smsalazar0319@gmail.com>
2024-11-08 09:50:42 -0500

    create

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- the script create new user in localhost with  password--
 
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_1'@'localhost' | grep -v 'WITH GRANT OPTION';
