582ad7e1a6a053a0d2fdf2e02eab4e665995c4ba
smorkl <smsalazar0319@gmail.com>
2024-11-15 17:47:56 -0500

    create

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,24 +2,23 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    if not tuple_a and tuple_b:
-        if len(tuple_a) == 1 and len(tuple_b) == 1:
-            tupla_c = (tuple_a[0] + tuple_b[0], 0)
+
+    tupla_c = (0, 0)
+    if len(tuple_a) == 1 and len(tuple_b) == 1:
+        tupla_c = (tuple_a[0] + tuple_b[0], 0)
+        return tupla_c
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 1:
+            tupla_c = (tuple_a[0] + tuple_b[0], tuple_b[1])
             return tupla_c
-        if len(tuple_a) < 2:
-            if len(tuple_a) == 1:
-                tupla_c = (tuple_a[0] + tuple_b[0], tuple_b[1])
-                return tupla_c
-            return tuple_b
-        elif len(tuple_b) < 2:
-            if len(tuple_b) == 1:
-                tupla_c = (tuple_a[0] + tuple_b[0], tuple_a[1])
-                return tupla_c
-            return tuple_a
-        elif len(tuple_a) == 0 and len(tuple_b) == 0:
-            tupla_c = (0, 0)
+        return tuple_b
+    elif len(tuple_b) < 2:
+        if len(tuple_b) == 1:
+            tupla_c = (tuple_a[0] + tuple_b[0], tuple_a[1])
             return tupla_c
-        else:
-            tupla_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-            return tupla_c
-    return tupla_c
+        return tuple_a
+    elif len(tuple_a) == 0 and len(tuple_b) == 0:
+        tupla_c = (0, 0)
+        return tupla_c
+    else:
+        return tupla_c
