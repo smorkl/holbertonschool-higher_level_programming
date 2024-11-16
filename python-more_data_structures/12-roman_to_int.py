ae69b864ed71397c53c3f75e165335214dc6498e
smorkl <smsalazar0319@gmail.com>
2024-11-15 19:07:20 -0500

    create

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or not roman_string:
         return 0  # Handle non-string or empty input
@@ -16,15 +18,14 @@ def roman_to_int(roman_string):
     prev_value = 0
 
     for char in roman_string[::-1]:
-        value = roman_dict.get(char, 0)  # Use get() to handle invalid characters
+        value = roman_dict.get(char, 0)
 
         if value < prev_value and prev_value not in (10, 100, 1000):
-            # Handle invalid subtractions (except for I, X, C before V, L, D)
             return 0
         elif value <= prev_value:
             num += value
         else:
-            num -= value  # Handle subtractions (I, X, C before V, L, D)
+            num -= value
         prev_value = value
 
-    return num if 1 <= num <= 3999 else 0  # Ensure valid range (1-3999)
+    return num if 1 <= num <= 3999 else 0
