559a845befcb7dc99fc9fc6e05eb710c947791c7
smorkl <smsalazar0319@gmail.com>
2024-11-18 14:00:12 -0500

    create

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -4,11 +4,12 @@ import MySQLdb
 from sys import argv
 
 # The code should not be executed when imported
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    )
 
     # It gives us the ability to have multiple seperate working environments
     # through the same connection to the database.
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -7,17 +7,20 @@ import MySQLdb
 from sys import argv
 
 # The code should not be executed when imported
-if __name__ == '__main__':
+if __name__ == "__main__":
     # make a connection to the database
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(
+        host="localhost", port=3306, user=argv[1], passwd=argv[2], db=argv[3]
+    )
 
     # It gives us the ability to have multiple seperate working environments
     # through the same connection to the database.
     cur = db.cursor()
 
-    cur.execute("SELECT * FROM states WHERE name\
-                LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states WHERE name\
+                LIKE BINARY 'N%' ORDER BY id ASC"
+    )
 
     rows = cur.fetchall()
     for i in rows:
