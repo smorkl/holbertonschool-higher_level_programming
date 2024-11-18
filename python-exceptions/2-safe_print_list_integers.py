038e22e2041adb6bbe2bc2f74192916404974d4c
smorkl <smsalazar0319@gmail.com>
2024-11-17 19:40:00 -0500

    create

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -12,6 +12,6 @@ def safe_print_list_integers(my_list=[], x=0):
             else:
                 break
         except (ValueError, TypeError, IndexError):
-            pass
+            continue
     print()
     return count
