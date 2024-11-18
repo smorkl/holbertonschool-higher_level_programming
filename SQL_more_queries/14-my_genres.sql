7fbeaca0e6e7409b300606b6241ce4ca78585899
smorkl <smsalazar0319@gmail.com>
2024-11-17 23:51:59 -0500

    create

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,4 +1,4 @@
--- Write a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter
+-- Write a script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter. --
 
 SELECT name FROM tv_genres
 JOIN tv_show_genres ON id=tv_show_genres.genre_id
